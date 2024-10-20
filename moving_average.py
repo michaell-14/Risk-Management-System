f553dbfe9af0747781826e658ed2c4f91071ae09
michaell-14 <121648960+michaell-14@users.noreply.github.com>
2024-10-20 15:42:47 -0700

    Update moving_average.py
    EMA added and graphing

--- moving_average.py
+++ moving_average.py
@@ -5,25 +5,54 @@ import test_api as api
 
 
 # Get stock data for "stock"
-api.ticker = "NVDA"  # Manual override
+api.ticker = "^GSPC"  
 ticker_price = api.get_current_stock_price(api.ticker) #will return the stock returns for the last month
 ticker_history = api.get_stock_history(api.ticker, "1mo")
 
 ticker_close = ticker_history["Close"]
 ticker_close_list = ticker_close.tolist()
-print("close: ", ticker_close_list) #this will return the closing prices for the last month in a list
+#this will return the closing prices for the last month in a list
 
 ticker_average = np.mean(ticker_close_list)
-print("average: ", ticker_average) #this will return the average closing price for the last month
+ #this will return the average closing price for the last month
+
+#standard deviation
+ticker_std = np.std(ticker_close_list)
 
 
-#Simple Moving Average (SMA)
-#SMA = (Sum of Price from N periods) / N
+print("Close Prices: ", ticker_close_list)  # This will return the stock closing prices for the last month
+print("Average Closing Price: ", ticker_average)  # This will return the average closing price for the last month
+print("Standard Deviation: ", ticker_std)  # This will return the standard deviation of the closing prices
 
-n = len(ticker_close_list) #number of trading days --> this would give N = the period called in function
 
-sma_stock = sum(ticker_close_list)/n
-print("Simple Moving Average: ", sma_stock)
 
-#Exponential Moving Average (EMA)
+# Simple Moving Average (SMA)
+# SMA = (Sum of Price from N periods) / N
+sma_stock = ticker_close.rolling(window=20).mean()
+print("Simple Moving Average: ", sma_stock.iloc[-1])
 
+#we dont use sma_stock because ema_stock is the exponential moving average and is more responsive to price changes
+
+# Exponential Moving Average (EMA)
+# EMA = (Price_today * k) + (EMA_yesterday * (1 - k))
+# where k = 2 / (N + 1)
+s = 2 / (len(ticker_close_list) + 1)
+ema_stock = [ticker_close_list[0]]  # Initialize EMA with the first closing price
+
+for i in range(1, len(ticker_close_list)):
+    ema_stock.append((ticker_close_list[i] - ema_stock[-1]) * s + ema_stock[-1])
+
+# Convert EMA list to a pandas Series
+ema_stock_series = pd.Series(ema_stock, index=ticker_close.index)
+ema_stock = pd.Series(ema_stock, index=ticker_close.index) #converting the list to a pandas series; reversing what I did in the beginning
+print("Exponential Moving Average: ", ema_stock)
+
+# Plotting the data
+plt.figure(figsize=(14, 7))
+plt.plot(ticker_close.index, ticker_close, label=f'{api.ticker} Close Prices')
+plt.plot(ticker_close.index, ema_stock, label=f'{api.ticker} EMA')
+plt.title(f'{api.ticker} Historical Prices with SMA and EMA')
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.legend()
+plt.show()
