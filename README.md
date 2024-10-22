d635c48157e2b5300b7055d6d29f170fe9b22d3e
michaell-14 <121648960+michaell-14@users.noreply.github.com>
2024-10-22 09:58:52 -0700

    stuff

--- README.md
+++
@@ -1,3 +0,0 @@
-# Stocks Project
-Its a work in progress
-- ESP32 stuff to come soon
---
+++ python_files/monte_c_plot.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import monte_carlo as mc
+
+print(mc.api.ticker)
